a2aa0003a21d47487518c57c5302a100355140b2
marcocnc <marcocancelliere56@gmail.com>
2023-05-16 16:44:08 +0200

    printed hotels name and descriptions

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php 
+
+/*
+Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate in modo graduale.
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+*/
+
+
+
+// Array hotel
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>PHP Hotel</title>
+</head>
+<body>
+    
+<h1>Test</h1>
+
+<ul>
+    <?php 
+        foreach($hotels as $hotel){
+            foreach ($hotel as $key => $value) {
+                echo "<li class='text-white'>$key: $value</li>";
+            }
+        }
+    ?>
+</ul>
+
+
+</body>
+</html>
