ab9f22ebffd73b69141315ad2fa369b61e1c3ad7
marcocnc <marcocancelliere56@gmail.com>
2023-05-16 18:08:13 +0200

    pushed table (to fix)

--- index.php
+++ index.php
@@ -53,6 +53,15 @@ $hotels = [
 
 
 
+// <ul>
+    // <?php 
+    //     foreach($hotels as $hotel){
+    //         foreach($hotel as $key => $element){
+    //             echo "<li>$key : $element</li>";
+    //         }
+    //     }
+    // ?>
+// </ul>
 
 ?>
 
@@ -65,22 +74,38 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <title>PHP Hotel</title>
 </head>
 <body>
-    
-<h1>Test</h1>
 
-<ul>
-    <?php 
-        foreach($hotels as $hotel){
-            foreach ($hotel as $key => $value) {
-                echo "<li class='text-white'>$key: $value</li>";
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nome Hotel</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza dal centro</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <?php 
+            foreach($hotels as $hotel){
+                foreach($hotel as $element){
+                    echo "<td> $element </td>";
+                }
             }
-        }
-    ?>
-</ul>
+        ?>
+    </tr>
+   
+  </tbody>
+</table>
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
